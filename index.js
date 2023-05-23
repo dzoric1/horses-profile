34d75fdb353593d31ac5e842c00638b04b7a2c1d
dzoric1 <Gerasimov2099@mail.ru>
2023-05-23 22:05:13 +0800

    test fixs

---
+++ index.js
@@ -0,0 +1,12 @@
+const asideLeftHandle = document.querySelector('.main__aside-handle')
+const asideLeft = document.querySelector('.main__aside-wrapper')
+const asideLeftClose = document.querySelector('.main__aside-wrapper-close')
+
+asideLeftHandle.addEventListener('mousedown', () => {
+  asideLeft.classList.add('main__aside-wrapper--show')
+})
+
+asideLeftClose.addEventListener('mousedown', () => {
+  asideLeft.classList.remove('main__aside-wrapper--show')
+})
+
